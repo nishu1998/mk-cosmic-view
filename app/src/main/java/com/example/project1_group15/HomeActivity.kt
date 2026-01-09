627f4cd868bee626c679126e12f8665567592ba5
Nishant gautam <79414961+nishu1998@users.noreply.github.com>
2026-01-09 16:28:43 +0530

    (manifest): set HomeActivity as launcher and deprecate MainActivity. UI(home): implement full-screen scrollable home layout with APOD hero

---
+++ app/src/main/java/com/example/project1_group15/HomeActivity.kt
@@ -0,0 +1,16 @@
+package com.example.project1_group15
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.project1_group15.databinding.ActivityHomeBinding
+
+
+class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}

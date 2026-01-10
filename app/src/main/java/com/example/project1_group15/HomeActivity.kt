5db75f627cc1bf37bcdde9aced840c2600b617f5
Nishant gautam <79414961+nishu1998@users.noreply.github.com>
2026-01-10 16:53:26 +0530

    (home): connect floating menu button to navigation drawer - Wired floating menu button to open DrawerLayout - Prepared home screen for navigation menu integration

--- app/src/main/java/com/example/project1_group15/HomeActivity.kt
+++ app/src/main/java/com/example/project1_group15/HomeActivity.kt
@@ -1,16 +1,30 @@
 package com.example.project1_group15
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.project1_group15.databinding.ActivityHomeBinding
+import com.google.android.material.navigation.NavigationView
 
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        drawerLayout = binding.drawerLayout
+
+        setupMenuButton()
+
+    }
+
+    private fun setupMenuButton() {
+        binding.btnMenu.setOnClickListener {
+            drawerLayout.open()
+        }
     }
 }
